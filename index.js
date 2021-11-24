4ba61e817f3399b2831ae2e57dee3bc6e50c5314
JonnasWillian <jonnasnogueira2@gmail.com>
2021-11-24 05:09:34 -0300

    'Backend2.4'

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function ReservaDeEmergencia(){
 
     if (Correcao === 5 ){
         text.innerHTML = (`Olá, você possui um valor total de R$: ${liquido} mensal`);
-        text2.innerHTML = (`Falta R$:${Objetivo - Caixa} se continuar reservando patrimonio assim, chegará ao seu objetivo em ${Math.ceil(tempo)} meses`);
+        text2.innerHTML = (`Falta R$:${Objetivo - Caixa} se continuar reservando patrimônio assim, chegará ao seu objetivo em ${Math.ceil(tempo)} meses`);
         Pobjetivo.innerHTML = (``)
         Psalario.innerHTML = (``)
         Pdespesa.innerHTML = (``)
@@ -102,18 +102,69 @@ function RendaFixa(){
     var cdi = document.getElementById("cdi")
     var duracao = document.getElementById("duracao")
     var valor = document.getElementById("valor")
+    var Correcao = 0;
+    //var imposto = document.getElementById("imposto")
 
     //Conexão com o HTML
     var CalculoCdi = document.getElementById("CdiCalculo")
+    var Objetivo = document.getElementById("Objetivo")
+    var Pcdi = document.getElementById("Pcdi")
+    var Pduracao = document.getElementById("Pduracao")
+    var Pvalor = document.getElementById("Pvalor")
 
     //conversão
     var Cdi = Number(cdi.value)
-    var Duracao = Number(duracao.value)
-    var Valor = Number(valor.value)
+    const CalculoDoCdi = Cdi <= 0
+        ?Pcdi.innerHTML=("Você Tem Que Possuir Uma Rentabilidade Maior Que 0")
+        :Correcao = Correcao + 1
 
-    //Calculo
+    var Duracao = Number(duracao.value)
+    const TemDuracao = Duracao <= 0
+        ?Pduracao.innerHTML=("A duração Do Investimento Tem Que Ser Maior Que 0")
+        :Correcao = Correcao + 1
+
+    var Valor = Number(valor.value)
+    const ValorDoInvestimento = Valor <= 0
+        ?Pvalor.innerHTML=("Valor Do Investimento Tem Que Ser Maior Que 0")
+        :Correcao = Correcao + 1
+    //var Imposto = Number(imposto.value)
+
+    //Cálculo
+    var rendimento = Cdi / 12;
+    
+    for (var contador = 0; contador < Duracao; contador++){
+        var soma = (Valor * rendimento) /100;
+        Valor = Valor + soma
+    }
 
     //Impressão De Valores
-    var ImprimiCdi = (`Olá, você possui um valor total de R$: ${Cdi} mensal, e ira demorar ${Duracao} meses para chegar ao seu objetivo`);
-    CalculoCdi.innerHTML = ImprimiCdi;
+    if (Correcao === 3 ){
+        CalculoCdi.innerHTML = (`Olá, o juros do CDI seria R$: ${rendimento}% mensal, você conseguiria ${Valor} até o final do tempo proposto`);
+        Objetivo.innerHTML = (``)
+        Pcdi.innerHTML = (``)
+        Pvalor.innerHTML = (``)
+        Pduracao.innerHTML = (``)
+    }else {
+        Objetivo.innerHTML = ("Por favor, preencha os campos corretamente")
+        CalculoCdi.innerHTML = ("")
+    }   
+}
+
+//Limpar Campos
+function limpar1(){
+    document.getElementById("Objetivo").value = "";
+    document.getElementById("CdiCalculo").value = "";
+    document.getElementById("Pcdi").value = "";
+    document.getElementById("Pvalor").value = "";
+    document.getElementById("Pduracao").value = "";
+    var CalculoCdi = document.getElementById("CdiCalculo")
+    var Objetivo = document.getElementById("Objetivo")
+    var Pcdi = document.getElementById("Pcdi")
+    var Pduracao = document.getElementById("Pduracao")
+    var Pvalor = document.getElementById("Pvalor")
+    CalculoCdi.innerHTML = ("");
+    Objetivo.innerHTML = ("");
+    Pcdi.innerHTML = (``);
+    Pduracao.innerHTML = (``);
+    Pvalor.innerHTML = (``);
 }
